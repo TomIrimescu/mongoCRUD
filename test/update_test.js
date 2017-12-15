60b53a063650502b0c312cee8b6f29755f784481
TomIrimescu <tom@irimescu.com>
2017-12-15 18:30:53 -0500

    fleshed out update test with 5 update methods

--- test/update_test.js
+++ test/update_test.js
@@ -1,1 +1,43 @@
-// TODO add code
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Updating records", () => {
+  let joe;
+
+  beforeEach(done => {
+    joe = new User({ name: "Joe" });
+    joe.save().then(() => done());
+  });
+
+  function assertName(operation, done) {
+    operation.then(() => User.find({})).then(users => {
+      assert(users.length === 1);
+      assert(users[0].name === "tomcat");
+      done();
+    });
+  }
+
+  it("instance type using set n save", done => {
+    joe.set("name", "tomcat"); // this operation is only done in memory
+    assertName(joe.save(), done); // this saves the changes to the db
+  });
+
+  it("A model instance can update", done => {
+    assertName(joe.update({ name: "tomcat" }), done);
+  });
+
+  it("A model class can update", done => {
+    assertName(User.update({ name: "Joe" }, { name: "tomcat" }), done);
+  });
+
+  it("A model class can update one record", done => {
+    assertName(
+      User.findOneAndUpdate({ name: "Joe" }, { name: "tomcat" }),
+      done
+    );
+  });
+
+  it("A model class can find a record with an Id and update", done => {
+    assertName(User.findByIdAndUpdate(joe._id, { name: "tomcat" }), done);
+  });
+});
